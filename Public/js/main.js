33caa34c9f37cb82c9401336c1fad178c71b6b2c
Yordan Dimitrov <dadanchi@abv.bg>
2017-05-05 17:35:03 +0300

    firebase stuff

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,1 +1,12 @@
-console.log(firebase.database());
+var ref = firebase.database();
+var name = "gosho";
+var bookId = 1;
+ref.ref("Books/" + bookId).set({
+    Name: name,
+    Author: "Pesho"
+});
+
+
+ref.ref("Books/" + bookId).once("value").then(snapshot => {
+    console.log(snapshot.val().Name);
+})
