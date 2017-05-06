7d8e2e1b677125bedd2165748ce22ab3bcc93760
lazarlyutakov <lazarlyutakov@gmail.com>
2017-05-06 14:00:30 +0300

    signUp test function

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,12 +1,37 @@
-var ref = firebase.database();
-var name = "gosho";
-var bookId = 1;
-ref.ref("Books/" + bookId).set({
-    Name: name,
-    Author: "Pesho"
-});
 
-ref.ref("Books/" + bookId).once("value").then(snapshot => {
-    console.log(typeof(snapshot));
-    console.log(snapshot.val().Name);
+var root = null;
+var useHash = true; // Defaults to: false
+var hash = '#!'; // Defaults to: '#'
+
+const signUp = function(){
+
+    let dbReference = firebase.database();
+    let username = $('#username-input').val();
+    let password = $('#password-input').val();
+    let userId = getNextId();
+
+    dbReference.ref('Library/Users/' + userId).set({
+        password :password,
+        username : username
+    });
+
+};
+
+var router = new Navigo(root, useHash, hash);
+
+router.on('/signup', signUp);
+
+
+firebase.database().ref("Library/Users/" + 0).once("value").then(snapshot => {
+    console.log(snapshot.val().username);
 })
+
+
+
+const getNextId = (function () {
+		let counter = 0;
+		return function () {
+			counter += 1;
+			return counter;
+		};
+	})();
