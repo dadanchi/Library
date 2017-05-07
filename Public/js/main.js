990901d6e842eeea7830cf1fa41a4d5f5bc2db9a
Martin Martinov <martinboykov@mail.bg>
2017-05-07 18:23:46 +0300

    about and contacts

--- Public/js/main.js
+++ Public/js/main.js
@@ -5,15 +5,17 @@ var hash = '#!'; // Defaults to: '#'
 var router = new Navigo(root, useHash, hash);
 
 router.on({
-        "home": homeController.load,
-        "books/allCategories": allCategoriesController.load,
-        "books/Category#1": categoryOneController.load,
-        "books/Category#2": categoryTwoController.load,
-        "books/Category#3": categoryThreeController.load,
-        "auth": userController.load,
-        "signup": userController.signUp,
-        "logout": userController.logout,
-    })
+    "home": homeController.load,
+    "books/allCategories": allCategoriesController.load,
+    "books/Category#1": categoryOneController.load,
+    "books/Category#2": categoryTwoController.load,
+    "books/Category#3": categoryThreeController.load,
+    "about": aboutController.load,
+    "contacts": contactsController.load,
+    "auth": userController.load,
+    "signup": userController.signUp,
+    "logout": userController.logout,
+})
     .resolve();
 
 
@@ -23,8 +25,6 @@ router.on({
 //         console.log(u.val().books);
 //     })
 // });
-
-
 
 
 window.onload = () => location.hash = "/home";
