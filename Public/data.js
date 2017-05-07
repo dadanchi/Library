7b2ee39b69a929c4ceb2c8284571920843e11c37
Yordan Dimitrov <dadanchi@abv.bg>
2017-05-07 16:21:50 +0300

    list all books

--- Public/data.js
+++ Public/data.js
@@ -10,14 +10,28 @@ const data = (() => {
             return this.getBooks().then(books => {
                 let categories = [];
                 let resultCategories = [];
+                let i = 0;
 
                 books.forEach(b => {
                     if (!categories.hasOwnProperty(b.genre)) {
-                        categories[b.genre] = { name: b.genre };
-                        resultCategories.push({ name: b.genre });
+                        categories[b.genre] = {
+                            name: b.genre,
+                            key: i
+                        };
+
+                        i++
+
+                        resultCategories.push({
+                            name: b.genre,
+                            books: []
+                        });
                     }
+
+                    let currentIndex = categories[b.genre].key;
+                    resultCategories[currentIndex].books.push(b);
                 });
 
+                console.log(resultCategories);
                 return resultCategories;
             });
         }
